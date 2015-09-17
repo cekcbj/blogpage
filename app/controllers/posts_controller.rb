4eb37c4bfea5f4427c88e93d1983e509ac619ca2
Byronj <cekcbj@gmail.com>
2015-09-17 08:58:19 -0500

    blog assignment

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  def list
+    @posts = Post.all
+  end
+
+  def detail
+    @post = Post.find params[:id]
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+
+    @post = Post.new params.require(:post).permit(:title, :body)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    @post = Post.find params[:id]
+    if @post.update params.require(:post).permit(:title, :body)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @post = Post.find params[:id]
+    @post.destroy
+    redirect_to root_path
+  end
+end
